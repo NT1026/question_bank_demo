54dce4d7e79868b337bc92e5adc0407ea0fb066d
NT1026 <nelson20020222@gmail.com>
2025-09-08 00:53:42 +0800

    feat: 新增 教師新增多個使用者 功能

--- api/routers/teacher_user_page.py
+++ api/routers/teacher_user_page.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends, Form, Request
+from csv import DictReader
+from fastapi import APIRouter, Depends, File, Form, Request, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from io import StringIO
 
 from .depends import get_current_user
 from api.response import (
@@ -53,7 +55,7 @@ async def user_create(
     "/user/create",
     response_class=HTMLResponse,
 )
-async def user_create_post(
+async def single_user_create_post(
     request: Request,
     username: str = Form(...),
     password: str = Form(...),
@@ -65,7 +67,7 @@ async def user_create_post(
     新增使用者頁面的 POST 請求處理
     - 未登入使用者無法進入新增使用者頁面，會被導向首頁
     - 已登入使用者，且使用者角色為非老師，無法進入新增使用者頁面，會回應 403 錯誤
-    - 已登入使用者，且使用者角色為老師，可提交新增使用者請求
+    - 已登入使用者，且使用者角色為老師，可提交新增單一使用者請求
     """
     # Check if user is teacher
     if not current_user:
@@ -82,7 +84,7 @@ async def user_create_post(
             {
                 "request": request,
                 "current_user": current_user,
-                "error": f"使用者帳號 {username} 已存在",
+                "error_single": f"使用者帳號 {username} 已存在",
             },
         )
 
@@ -100,7 +102,78 @@ async def user_create_post(
         {
             "request": request,
             "current_user": current_user,
-            "success": f"已建立新使用者帳號：{username}",
+            "success_single": f"已建立新使用者帳號：{username}",
+        },
+    )
+
+
+@router.post(
+    "/users/create",
+    response_class=HTMLResponse,
+)
+async def multiple_user_create_post(
+    request: Request,
+    file: UploadFile = File(...),
+    current_user=Depends(get_current_user),
+):
+    """
+    新增使用者頁面的 POST 請求處理
+    - 未登入使用者無法進入新增使用者頁面，會被導向首頁
+    - 已登入使用者，且使用者角色為非老師，無法進入新增使用者頁面，會回應 403 錯誤
+    - 已登入使用者，且使用者角色為老師，可提交新增多個使用者請求
+    """
+    # Check if user is teacher
+    if not current_user:
+        return _302_REDIRECT_TO_HOME
+
+    if current_user.role != Role.TEACHER:
+        return _403_NOT_A_TEACHER
+
+    # Read csv file
+    content = await file.read()
+    f = StringIO(content.decode("utf-8"))
+    reader = DictReader(f)
+
+    # Create users
+    fail_to_add = []
+    for row in reader:
+        username = row["username"].strip()
+        password = row["password"].strip()
+        name = row["name"].strip()
+        role = row["role"].strip()
+
+        # Check if user with the same username already exists
+        user_to_add = await UserCrud.get_by_username(username)
+        if not user_to_add:
+            newUser = UserSchema.UserCreate(
+                username=username,
+                password=password,
+                name=name,
+                role=role,
+            )
+            newUser.password = get_password_hash(newUser.password)
+            await UserCrud.create(newUser)
+        else:
+            fail_to_add.append(username)
+
+    # Render user_create.html
+    if fail_to_add:
+        user_list = ','.join(fail_to_add)
+        return templates.TemplateResponse(
+            "user_create.html",
+            {
+                "request": request,
+                "current_user": current_user,
+                "error_multiple": f"以下使用者新增失敗：{user_list}",
+            },
+        )
+
+    return templates.TemplateResponse(
+        "user_create.html",
+        {
+            "request": request,
+            "current_user": current_user,
+            "success_multiple": f"csv 中所有使用者皆新增完成",
         },
     )
 
